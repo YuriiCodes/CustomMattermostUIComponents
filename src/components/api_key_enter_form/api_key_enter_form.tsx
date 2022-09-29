63a7158ab9ac66ab3c7c3bb1b23524b971d33025
YuriiCodes <yuriipidlisnyi2021@gmail.com>
2022-09-29 14:23:34 +0300

    added local state for api token enter form

--- src/components/api_key_enter_form/api_key_enter_form.tsx
+++ src/components/api_key_enter_form/api_key_enter_form.tsx
@@ -1,13 +1,16 @@
 import classes from "../customUIStyles.module.css";
 import InputField from "../input_field";
+import {useState} from "react";
 
 
 export default function ApiKeyEnterForm(): JSX.Element {
+    const [apiToken, setApiToken] = useState("");
+    console.log(apiToken);
     return (
         <div className={classes.addTicketForm}>
             <form>
                 <div className={classes.inputGroup}>
-                    <InputField name={"PipeDrive API key"} placeholder={""} type={"text"} required={true}/>
+                    <InputField name={"PipeDrive API key"} placeholder={""} type={"text"} required={true} setValue={setApiToken}/>
                     <button className={classes.submitBtn} type={"submit"}>Submit API key</button>
                 </div>
             </form>
